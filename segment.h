54346e83302f5bd75d75952106fd19ae4d417303
taggelos <aggelos5991@gmail.com>
2016-02-19 16:26:23 +0200

    diplh sundedemenh lista segment
    yolo swag

--- segment.h
+++ segment.h
@@ -18,7 +18,24 @@ class Segment{
 	Segment* prev;
 	public:
 		Segment(int n);
+		void setNext(Segment* n) {next = n;};
+		void setPrev(Segment* n) {prev = n;};
+		Segment* getPrev(){return prev;};
 		~Segment();
 };
 
+class doubleLinkedList
+{	
+	Segment* front;
+	Segment* back;
+	public:
+		doubleLinkedList()
+		{  front = NULL; back = NULL; }
+		~doubleLinkedList(){ destroyList();}
+		void appendSegmentFront(int x);
+		void appendSegmentBack(int x);
+		//void dispNodesForward();
+		//void dispNodesReverse();
+		void destroyList();
+};
 #endif
