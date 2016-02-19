14519da1065f340c567fb69d25d486d33a0dab10
helena19 <elemanama@gmail.com>
2016-02-19 17:13:34 +0200

    Revert "diplh sundedemenh lista segment"
    This reverts commit 54346e83302f5bd75d75952106fd19ae4d417303.

--- segment.cpp
+++ segment.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 
  Segment::Segment(int n){
- 	prev=next=NULL;
  	int count=0;
  	cars=new Car*[n];
  	for(int i=0;i<n;i++){ //dhmiourgia 0-5 autokinhtwn
@@ -20,54 +19,3 @@ Segment::~Segment(){
 	cout<<"Καταστροφή Segment"<<endl;
 }
 
-
-void doubleLinkedList::appendSegmentFront(int x)
-  {
-        Segment *n = new Segment(x);
-        if( front == NULL)
-        {
-            front = n;
-            back = n;
-        }
-        else
-        {
-            //front->prev = n;
-            front->setPrev(n);
-            //n->next = front;
-            n->setNext(front);
-            front = n;
-        }
-  }
-
-  void doubleLinkedList::appendSegmentBack(int x)
-  {
-        Segment *n = new Segment(x);
-        if( back == NULL)
-        {
-            front = n;
-            back = n;
-        }
-        else
-        {
-            //back->next = n;
-            back->setNext(n);
-            //n->prev = back;
-            n->setPrev(back);
-            back = n;
-        }
- 
-  }
- 
- void doubleLinkedList::destroyList()
-{
-    Segment *T = back;
-    while(T != NULL)
-    {
-        Segment *T2 = T;
-        //T = T->prev;
-        T=T->getPrev();
-        delete T2;
-    }
-    front = NULL;
-    back = NULL;
-}
